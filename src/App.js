811a50d301fec0e22ea4da5ad2d0eabd050fbc7f
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-11-17 16:38:21 +0100

    feat(app): add some components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class App extends Component {
+function SearchBar(props) {
+  return <input type="text" onChange={props.onSearch} />
+}
+
+function ListItem(props) {
+  return <div>{props.text}</div>
+}
+
+class App extends React.Component {
   render() {
+    var results = ['Item 1', 'Item 2']
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <SearchBar
+          onSearch={function(event) {
+            var value = event.target.value
+            console.log('new value :', value)
+          }}
+        />
+        {results.map(item => <ListItem text={item} />)}
       </div>
     )
   }
