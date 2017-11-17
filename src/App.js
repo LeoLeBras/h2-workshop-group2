75c6cdad1e137d9c6738c0bcce566e924467e92f
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-11-17 16:51:51 +0100

    feat(app): add search feature

--- src/App.js
+++ src/App.js
@@ -9,17 +9,29 @@ function ListItem(props) {
 }
 
 class App extends React.Component {
+  state = { searchQuery: undefined }
+
+  handleSearch = event => {
+    var value = event.target.value
+    this.setState({ searchQuery: value })
+  }
+
   render() {
+    console.log(this.state.searchQuery)
     var results = ['Item 1', 'Item 2']
     return (
       <div>
-        <SearchBar
-          onSearch={function(event) {
-            var value = event.target.value
-            console.log('new value :', value)
-          }}
-        />
-        {results.map(item => <ListItem text={item} />)}
+        <SearchBar onSearch={this.handleSearch} />
+        {results
+          .filter(item => {
+            if (this.state.searchQuery === undefined) {
+              return true
+            }
+            return item.includes(this.state.searchQuery)
+          })
+          .map(function(item) {
+            return <ListItem text={item} />
+          })}
       </div>
     )
   }
